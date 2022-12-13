5e8f36cc04ff48b6e6f62891e590a673aec8fc06
AKIBIRIO <ayubkibirio@gmail.com>
2022-12-13 20:13:43 +0300

    Added implementation of Java Three Dimension Arrays...

---
+++ src/JavaArrays/CodeFactory3DArray.java
@@ -0,0 +1,19 @@
+package JavaArrays;
+
+import java.util.Arrays;
+
+public class CodeFactory3DArray {
+    // adding an implementaion of a three dimension array
+
+    public static void main(String[] args) {
+        // signature
+        int[][] ticTacToeBoard = {{0,0,0},{0,0,0},{0,0,0}};
+
+        //loop through 3 d using enhanced for loop
+        for(int[] iBoard : ticTacToeBoard){
+            System.out.print(Arrays.toString(iBoard));
+        }
+        
+    }
+    
+}
