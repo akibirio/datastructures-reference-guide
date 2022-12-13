bafa63e173479a2d026a0151a8bcd6c7fc9fe80e
AKIBIRIO <ayubkibirio@gmail.com>
2022-12-13 20:01:33 +0300

    Added implementation of Java Tow Diemnsion Arrays...

--- src/JavaArrays/CodeFactory2DArray.java
+++ src/JavaArrays/CodeFactory2DArray.java
@@ -6,16 +6,16 @@ package JavaArrays;
 public class CodeFactory2DArray {
     // here I define a 2 dimension array. It's index based. Indes IDs start from 0.
     // arrays check if the length of the id is 0 to make sure you dont run to IndexOutOfBoundary exception
-  public static void main(String[] args) {
-    int i = 0;
-    int[] sizes = {24, 67, 89, 90, 102};
-    
-    //print all sizes one by one using enhanced for loop
-    for(int size : sizes){
-        System.out.println("Size at Position " + i + " of 2D array is : " +size);
-        i++;
-    }
-  }
+    public static void main(String[] args) {
+        int i = 0;
+        int[] sizes = {24, 67, 89, 90, 102};
+        
+        //print all sizes one by one using enhanced for loop
+        for(int size : sizes){
+            System.out.println("Size at Position " + i + " of 2D array is : " +size);
+            i++;
+        }
+      }
 
     
 }
